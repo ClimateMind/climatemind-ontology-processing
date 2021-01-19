0ae7cc3c776fd6579abaa6d555b3773e7776b89c
Kameron Rodrigues <kameronr@stanford.edu>
2021-01-19 12:11:43 -0800

    v 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="climatemind-ontology-processing",
-    version="1.0.1",
+    version="1.0.0",
     description="Climate Mind ontology processing code.",
     author="ClimateMind",
     url="https://github.com/ClimateMind/climatemind-ontology-processing",
