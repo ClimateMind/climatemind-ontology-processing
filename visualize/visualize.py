42624fba8bac2fc34aaca8b031a7a69d81ba5f75
Henry Nguyen <henry2833@gmail.com>
2021-03-18 09:17:08 -0700

    Refactored code. Make two copies of the graphs: one contains solutions, one without solutions.

--- visualize/visualize.py
+++ visualize/visualize.py
@@ -17,11 +17,13 @@ def custom_bfs(graph, start_node):
 
     def do_bfs(element):
         for in_edge in graph.in_edges(element, data=True):
+
             new_graph.add_edge(in_edge[0], in_edge[1], **in_edge[2])
             queue.append(in_edge[0])
         for out_edge in graph.out_edges(element, data=True):
             if 'risk solution' in graph.nodes[out_edge[1]]:
                 new_graph.add_edge(out_edge[0], out_edge[1], **out_edge[2], weight=10)
+                # We don't append to queue because don't want to explore further. solutions are terminal leaf nodes.
 
     do_bfs(start_node)
 
@@ -39,85 +41,113 @@ def filter_node(input_node):
     return 'myth' not in G1.nodes[input_node]
 
 
+def filter_node_no_solutions(input_node):
+    return filter_node(input_node) and 'risk solution' not in G1.nodes[input_node]
+
+
 # Only want causal edges in our graph
 def filter_edge(e1, e2):
     return True
     return G1[e1][e2]['type'] == 'causes_or_promotes'
 
 
-G = nx.subgraph_view(G1, filter_node=filter_node, filter_edge=filter_edge)
-# G = custom_bfs(G, 'increase in physical violence')
-# N = nx.nx_agraph.to_agraph(G)
+G_sln = nx.subgraph_view(G1, filter_node=filter_node, filter_edge=filter_edge)
+G_noslns = nx.subgraph_view(G1, filter_node=filter_node_no_solutions, filter_edge=filter_edge)
 
 
-# Holds the complete graph cytoscape data (node positions + labels) for each node with a personal value associated
-value_nodes = {node_tuple[0]: list() for node_tuple in G.nodes.data('personal_values_10', [None]) if
-               any(node_tuple[1])}
+def compute_graphs(G):
+    # Holds the complete graph cytoscape data (node positions + labels) for each node with a personal value associated
+    value_nodes = {node_tuple[0]: list() for node_tuple in G.nodes.data('personal_values_10', [None]) if
+                   any(node_tuple[1])}
 
-value_nodes = {'increase in physical violence': value_nodes['increase in physical violence']}
+    value_nodes = {'increase in physical violence': value_nodes['increase in physical violence']}
+    for value_key, cyto_elements in value_nodes.items():
 
-for value_key, cyto_elements in value_nodes.items():
-    # subtree = nx.DiGraph()
-    # for e1, e2, direction in nx.edge_bfs(G, value_key, 'ignore'):
-    #     if direction == 'reverse':
-    #         subtree.add_edge(e1, e2, **G[e1][e2])
-    #     elif direction == 'forward':
-    #         if 'risk solution' in G.nodes[e2] or True:
-    #             subtree.add_edge(e1, e2, **G[e1][e2])
+        subtree = custom_bfs(G, value_key)
+        for node in subtree.nodes:
+            nx.set_node_attributes(subtree, {node: G.nodes[node]})
 
-    subtree = custom_bfs(G, value_key)
-    nodes_to_remove = set()
-    for node in subtree.nodes:
-        nx.set_node_attributes(subtree, {node: G.nodes[node]})
-        # Debug checks
-        # if subtree.nodes[node].get('test ontology', None) != ['test ontology']:
-        #     raise RuntimeError("test ontology present in graph")
+            # Manual wrap text so graph is more condensed.
+            if len(subtree.nodes[node]['label']) > 20:
+                subtree.nodes[node]['label'] = textwrap.fill(subtree.nodes[node]['label'], 20)
 
-        # Manual wrap text so graph is more condensed.
-        if len(subtree.nodes[node]['label']) > 20:
-            subtree.nodes[node]['label'] = textwrap.fill(subtree.nodes[node]['label'], 20)
+        N = nx.nx_agraph.to_agraph(subtree)
 
-    N = nx.nx_agraph.to_agraph(subtree)
+        for node in subtree.nodes:
+            if 'risk' in subtree.nodes[node]:
+                connected_solutions = [sln[1] for sln in subtree.out_edges(node, data=True) if
+                                       'risk solution' in subtree.nodes[sln[1]]]
+                connected_solutions.append(node)
+                N.add_subgraph(connected_solutions, f"cluster_{node}")
+        N.edge_attr.update(directed=True)
+        N = N.unflatten(f'-f -l6')
+        # N.layout(prog='dot', args='-Gratio=compress -Gsize="30, 30" -Grankdir=BT -Gnodesep=0.01 -Nmargin="0.01,0.01"')
+        N.layout(prog='dot')
+        N.draw(f"{value_key}.png", "png")
+        N.write(f"{value_key}.dot")
 
-    N.edge_attr.update(directed=True)
-    N = N.unflatten(f'-f -l15')
-    N.layout(prog='dot', args='-Gratio=compress -Gsize="30, 30" -Grankdir=BT -Gnodesep=0.01 -Nmargin="0.01,0.01"')
-    # N.draw(f"{value_key}.png", "png")
-    N.write(f"{value_key}.dot")
+        total_cyto_data = nx.readwrite.json_graph.cytoscape_data(subtree)['elements']
+        cyto_nodes = total_cyto_data['nodes']
+        cyto_edges = total_cyto_data['edges']
 
-    total_cyto_data = nx.readwrite.json_graph.cytoscape_data(subtree)['elements']
-    cyto_nodes = total_cyto_data['nodes']
-    cyto_edges = total_cyto_data['edges']
+        for edge in cyto_edges:
+            if 'risk solution' in G.nodes[edge['data']['source']] or 'risk solution' in G.nodes[edge['data']['target']]:
+                edge['classes'] = 'solution-edge'
 
-    for edge in cyto_edges:
-        if 'risk solution' in G.nodes[edge['data']['source']] or 'risk solution' in G.nodes[edge['data']['target']]:
-            edge['classes'] = 'solution-edge'
+        # Extract x y positions using graphviz dot algo. Use x,y positions to make cytoscape graph
+        for node in cyto_nodes:
+            graphviz_node = N.get_node(node['data']['id'])
+            position = graphviz_node.attr.get('pos', []).split(',')
+            node['data'] = {**G.nodes[node['data']['id']], 'id': node['data']['id']}
+            node['position'] = {'x': int(float(position[0])), 'y': int(float(position[1]))}
 
-    # Extract x y positions using graphviz dot algo. Use x,y positions to make cytoscape graph
-    for node in cyto_nodes:
-        graphviz_node = N.get_node(node['data']['id'])
-        position = graphviz_node.attr.get('pos', []).split(',')
-        node['data'] = {**G.nodes[node['data']['id']], 'id': node['data']['id']}
-        node['position'] = {'x': int(float(position[0])), 'y': int(float(position[1]))}
+            if node['data']['id'] == value_key:
+                node['classes'] = 'tree_root'
 
-        if node['data']['id'] == value_key:
-            node['classes'] = 'tree_root'
+            if 'risk solution' in node['data']:
+                node['classes'] = 'risk-solution'
 
-        if 'risk solution' in node['data']:
-            node['classes'] = 'risk-solution'
+            if 'myth' in node['data']:
+                node['classes'] = 'myth'
 
-        if 'myth' in node['data']:
-            node['classes'] = 'myth'
+        cyto_elements[:] = list(cyto_nodes) + list(cyto_edges)
+    return value_nodes
 
-    cyto_elements[:] = list(cyto_nodes) + list(cyto_edges)
 
+all_graph_data_noslns = compute_graphs(G_noslns)
+all_graph_data_slns = compute_graphs(G_sln)
 app = dash.Dash(__name__)
 
+node_storage = dcc.Store(
+    id='node-storage',
+    storage_type='local',
+    data=dict(G_noslns.nodes(data=True))
+)
+
+edge_storage_data = {}
+
+for (e1, e2), edge_properties in dict(G1.out_edges).items():
+    if e1 not in edge_storage_data:
+        edge_storage_data[e1] = {}
+    edge_storage_data[e1][e2] = {**edge_properties, 'direction': 'forward'}
+
+for (e1, e2), edge_properties in dict(G1.in_edges).items():
+    if e1 not in edge_storage_data:
+        edge_storage_data[e1] = {}
+    edge_storage_data[e1][e2] = {**edge_properties, 'direction': 'reverse'}
+
+edge_storage = dcc.Store(
+    id='edge-storage',
+    storage_type='local',
+    data=edge_storage_data
+)
 app.layout = html.Div([
+    node_storage,
+    edge_storage,
     dcc.Dropdown(
         id='select',
         options=[
-            {'label': name, 'value': name} for name in value_nodes.keys()
+            {'label': name, 'value': name} for name in all_graph_data_slns.keys()
         ],
         style={'display': 'inline-block', 'width': '70%'}
     ),
@@ -136,30 +166,41 @@ app.layout = html.Div([
         style={'width': '70%', 'height': '800px'},
         elements=[],
         stylesheet=[
-            {
-                'selector': '.tree_root',
-                'style': {
-                    'background-color': 'blue',
-                    'width': '50',
-                    'height': '50'
-                }
-            },
+
             {
                 'selector': "edge",
                 'style': {
-                    'width': 3,
+                    'width': 1,
                     'line-color': '#ccc',
                     'target-arrow-color': 'red',
                     'target-arrow-shape': 'triangle',
                     'curve-style': 'bezier',
-                    'arrow-scale': 2
+                    'arrow-scale': 2,
+                    'line-opacity': 0.9
                 }
             }, {
                 'selector': 'node',
                 'style': {
                     'label': 'data(label)',
                     'text-wrap': 'wrap',
-                    'text-max-width': '10em'
+                    'text-max-width': '12em',
+                    'text-halign': 'center',
+                    'text-valign': 'center',
+                    'width': 'label',
+                    'height': 'label',
+                    'font-size': 20,
+                    'shape': 'round-rectangle',
+                    'background-color': 'orange'
+                }
+            }, {
+                'selector': 'node',
+                'style': {
+                    'padding': '10px',
+                }
+            }, {
+                'selector': '.tree_root',
+                'style': {
+                    'background-color': 'blue'
                 }
             }, {
                 'selector': '.risk-solution',
@@ -169,39 +210,47 @@ app.layout = html.Div([
             }, {
                 'selector': '.solution-edge',
                 'style': {
-                    'line-color': 'green'
+                    'line-color': 'green',
+                    'line-opacity': 0.3
                 }
             }
         ]
-    )
+    ),
+    html.P(id='output', style={'whiteSpace': 'pre-wrap'})
 ])
 
 
 @app.callback(
     dash.dependencies.Output('cytoscape-two-nodes', 'elements'),
-    [dash.dependencies.Input('select', 'value')]
+    [dash.dependencies.Input('select', 'value'), dash.dependencies.Input('solutions-check', 'value')]
 )
-def change_viewed_key(new_key):
+def change_viewed_key(new_key, view_slns):
     if new_key is None:
-        new_key = next(iter(value_nodes))
+        new_key = next(iter(all_graph_data_slns))
 
-    print(f"changing to: {new_key}")
-    return value_nodes[new_key]
+    print(f"changing to: {new_key}", view_slns)
+    return all_graph_data_slns[new_key] if view_slns else all_graph_data_noslns[new_key]
+
+
+app.clientside_callback(
+    dash.dependencies.ClientsideFunction(
+        namespace='clientside',
+        function_name='mouse_over_func'
+    ),
+    dash.dependencies.Output('output', 'children'),
+    dash.dependencies.Input('cytoscape-two-nodes', 'mouseoverNodeData'),
+    dash.dependencies.State('edge-storage', 'data')
+)
 
 
 @app.callback(
-    dash.dependencies.Output('cytoscape-two-nodes', 'stylesheet'),
-    [dash.dependencies.Input('solutions-check', 'value')],
-    [dash.dependencies.State('cytoscape-two-nodes', 'stylesheet')]
+    dash.dependencies.Output('edge-storage', 'data'),
+    dash.dependencies.Input('cytoscape-two-nodes', 'mouseoverNodeData'),
+    dash.dependencies.State('edge-storage', 'data')
 )
-def toggle_solution_view(yes_solution, stylesheet):
-    print('toggled')
-
-    for style in stylesheet:
-        if style['selector'] in ['.risk-solution', '.solution-edge']:
-            style['style']['display'] = 'element' if yes_solution else 'none'
-
-    return stylesheet
+def test_callback(node_data, edge_data):
+    print(node_data, '\n', edge_data)
+    return edge_data
 
 
 app.run_server(debug=True)
